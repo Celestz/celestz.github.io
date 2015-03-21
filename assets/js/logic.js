32251ba515b4f5b3d7aa53aad239ac1bbbfac2ce
Patrick@Envy-Win10 <i.am.celestz@gmail.com>
2015-03-22 03:34:27 +0800

    Updated with Grid Layout

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,31 @@
-$('body').scrollspy({ target: '#sidebar' })
+$('body').scrollspy({
+  target: '#sidebar'
+})
+$(document).ready(function() {
+  var $grid = $('#grid'),
+    $sizer = $grid.find('.shuffle__sizer');
+
+  $grid.shuffle({
+    itemSelector: '.grid-item',
+    sizer: $sizer
+  });
+
+  var $btns = $('#news-filter').children();
+  $btns.on('click', function() {
+    event.preventDefault();
+    var $this = $(this),
+      isActive = $this.hasClass('active'),
+      group = isActive ? 'all' : $this.data('group');
+
+    // Hide current label, show current label in title
+    if (!isActive) {
+      $('#news-filter .active').removeClass('active');
+    }
+
+    $this.toggleClass('active');
+
+    // Filter elements
+    $grid.shuffle('shuffle', group);
+  });
+
+});
