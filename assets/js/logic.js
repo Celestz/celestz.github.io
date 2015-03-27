0a18d8c31dbd361d7ba1b5e47e60b3bb18c1e495
Patrick@Envy-Win10 <i.am.celestz@gmail.com>
2015-03-28 01:42:58 +0800

    new

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -28,4 +28,21 @@ $(document).ready(function() {
     $grid.shuffle('shuffle', group);
   });
 
+  $(".nav-area li a").on('click', function(e) {
+     // prevent default anchor click behavior
+     e.preventDefault();
+
+     // animate
+     $('html, body').animate({
+         scrollTop: $(this.hash).offset().top
+       }, 1000, function(){
+
+         // when done, add hash to url
+         // (default click behaviour)
+         window.location.hash = this.hash;
+       });
+
+  });
+
+
 });
